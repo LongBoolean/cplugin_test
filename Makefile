517cb630f551bd4cf186349ce9c7b9845360b231
Nicholas Peterson <derstreber2@gmail.com>
2016-06-14 20:22:59 -0500

    implement loading plugins from a plugin directory

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all:
 	g++ -g -Wall -rdynamic -o cplugin_test main.cpp -ldl
-	g++ -g -Wall -shared -o pluginA.so -fPIC pluginA.cpp 
-	g++ -g -Wall -shared -o pluginB.so -fPIC pluginB.cpp 
+	g++ -g -Wall -shared -o ./plugins_bin/pluginA.so -fPIC pluginA.cpp 
+	g++ -g -Wall -shared -o ./plugins_bin/pluginB.so -fPIC pluginB.cpp 
 #	g++ -g -shared -fPIC -Wl,-soname,pluginA.so -o pluginA.so pluginA.cpp 
 	
 debug:
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <dlfcn.h>
+#include <dirent.h>
+#include <string.h>
+#include "shared.h"
 
 void 
 loadLib(void **handle, const char *lib_path)
@@ -36,6 +39,7 @@ struct plugin
 void 
 create_plugin(struct plugin *Plugin, const char* path)
 {
+    printf("Create plugin: %s\n", path);
     loadLib(&Plugin->handle, path);
     Plugin->setup = (void (*)(int))getLibFunc(Plugin->handle, "setup");
     Plugin->update = (void (*)())getLibFunc(Plugin->handle, "update");
@@ -68,24 +72,89 @@ remove_plugin(struct plugin *Plugin)
     }
 }
 
+bool
+check_str_inside(const char *haystack_str, const char *needle_str)
+{
+    bool result = false;
+    char haystack_char;
+    char needle_char;
+    int j = 0;
+    for(int i=0; ; i++)
+    {
+        needle_char = needle_str[j];
+        if(needle_char == '\0')
+        {
+            result = true;
+            break;
+        }
+        
+        haystack_char = haystack_str[i];
+        if(haystack_char == '\0') break;
+
+        //printf("haystack_char: %c, needle_char: %c\n", haystack_char, needle_char);
+        if(haystack_char == needle_char)
+        {
+            j++;
+        }
+        else
+        {
+            j = 0;
+        }
+
+    }
+    return result;
+}
+
+#define NUM_PLUGIN_MAX 16
+
+global_variable const char *PLUGIN_DIR = "./plugins_bin/";
+global_variable int num_plugins = 0;
+
 int 
 main()
 {
-    struct plugin *PluginA = (struct plugin*)malloc(sizeof(struct plugin));
-    struct plugin *PluginB = (struct plugin*)malloc(sizeof(struct plugin));
+    struct plugin *plugins_arr[NUM_PLUGIN_MAX];
 
-
-    //todo(nick): find better way to deturmine full path
-    create_plugin(PluginA, "./pluginA.so");
-    create_plugin(PluginB, "./pluginB.so");
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(PLUGIN_DIR);
+    if(d)
+    {
+        while((dir = readdir(d)) != NULL)
+        {
+            if((dir->d_type == DT_REG) &&
+               (check_str_inside(dir->d_name,".so")))
+            {
+                //add plugin
+                if(num_plugins < NUM_PLUGIN_MAX)
+                {
+                    plugins_arr[num_plugins] = (struct plugin*)malloc(sizeof(struct plugin));
+                    char path[64];
+                    strcpy(path, PLUGIN_DIR);
+                    strcat(path, dir->d_name);
+                    create_plugin(plugins_arr[num_plugins], path);
+                    num_plugins++;
+                }
+                else
+                {
+                    printf("Could not add plugin: %s, to many plugins.\n", dir->d_name);
+                }
+            }
+        }
+    }
 
     for(int i=0; i<10; i++)
     {
-        run_plugin(PluginA, i);
-        run_plugin(PluginB, i);
+        for(int j=0; j<num_plugins; j++)
+        {
+            run_plugin(plugins_arr[j], i);
+        }
         printf("******************************\n");
     }
 
-    remove_plugin(PluginA);
-    remove_plugin(PluginB);
+    for(int j=0; j<num_plugins; j++)
+    {
+        remove_plugin(plugins_arr[j]);
+    }
+
 } 
